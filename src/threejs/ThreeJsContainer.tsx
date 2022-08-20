04ebbbdcd904cbe38ceac9aceddfc1786f2469af
Jakub-Izbicki <jakub.izbicki@wundermanthompson.com>
2022-08-20 20:50:38 +0200

    Import blender glb

--- src/threejs/ThreeJsContainer.tsx
+++ src/threejs/ThreeJsContainer.tsx
@@ -1,13 +1,15 @@
 import './ThreeJsContainer.css';
 import {useRef, useState} from "react";
-import {Canvas, ThreeEvent, useFrame} from "@react-three/fiber";
+import {Canvas, ThreeEvent, useFrame, useLoader} from "@react-three/fiber";
 import * as THREE from 'three';
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+import {OrbitControls, useGLTF} from "@react-three/drei";
 
 // @ts-ignore
 const cartesianProduct = (...a) => a.reduce((a, b) => a.flatMap(d => b.map(e => [d, e].flat())));
 
 export default function ThreeJsContainer() {
-  let sideSize = 2;
+  let sideSize = 1;
 
   const boxes = cartesianProduct([...Array(sideSize).keys()], [...Array(sideSize).keys()])
     .map((values: [number, number]) => new THREE.Vector3(values[0], values[1], 0))
@@ -15,10 +17,19 @@ export default function ThreeJsContainer() {
 
   return (
       <div className={'master-container'}>
-        <Canvas>
-          <ambientLight/>
-          <pointLight position={[10, 10, 10]}/>
-          {boxes}
+        <Canvas shadows={true}>
+          <ambientLight intensity={0.1}/>
+          <color attach="background" args={['#000000']} />
+          {/*<pointLight position={[10, 10, 10]}/>*/}
+
+          <directionalLight position={[-10, 10, 5]}  castShadow={true}>
+          </directionalLight>
+
+          <OrbitControls/>
+
+          {/*{boxes}*/}
+          <TileColumn/>
+          <Ground/>
         </Canvas>
       </div>
   );
@@ -40,11 +51,13 @@ export function Box(props: JSX.IntrinsicElements['mesh']) {
     setClicked(clicked)
   }
 
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+  // useFrame((state, delta) => (ref.current.rotation.x += 0.01));
 
   return (
       <mesh
           {...props}
+          castShadow={true}
+          receiveShadow={true}
           ref={ref}
           scale={clicked ? 1.5 : 1}
           onClick={(event) => onClick(event, !clicked)}
@@ -53,5 +66,35 @@ export function Box(props: JSX.IntrinsicElements['mesh']) {
         <boxGeometry args={[1, 1, 1]}/>
         <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'}/>
       </mesh>
+  );
+}
+
+function Ground(props: JSX.IntrinsicElements['mesh']) {
+  return (
+      <mesh
+          {...props}
+          castShadow={true}
+          receiveShadow={true}
+          position={[0, -0.05, 0]}>
+        <boxGeometry args={[15, 0.1, 15]}/>
+        <meshStandardMaterial color={'grey'}/>
+      </mesh>
+  );
+}
+
+function TileColumn(props: JSX.IntrinsicElements['group']) {
+  const tiles01 = useGLTF('models/tiles01.glb');
+
+  return (
+      <group {...props} dispose={null}>
+        <mesh
+            castShadow={true}
+            receiveShadow={true}
+            geometry={tiles01.nodes.Cube.geometry}
+            material={tiles01.materials["Material.010"]}
+            position={[0, 0.5, 0]}
+        >
+        </mesh>
+      </group>
   );
 }
